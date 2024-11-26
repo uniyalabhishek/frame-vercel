fa077c79a91dad426c780075507ef74ea9110e46
Abhishek Uniyal <56363630+uniyalabhishek@users.noreply.github.com>
2024-11-26 20:04:11 +0530

    git init frame demo
    Signed-off-by: Abhishek Uniyal <56363630+uniyalabhishek@users.noreply.github.com>

---
+++ api/index.tsx
@@ -0,0 +1,49 @@
+import { Button, Frog } from "frog";
+import { devtools } from "frog/dev";
+import { handle } from "frog/next";
+import { serveStatic } from "frog/serve-static";
+
+export const app = new Frog({
+  assetsPath: "/",
+  basePath: "/api",
+  title: "Hello World Frame",
+});
+
+app.frame("/", (c) => {
+  const { status } = c;
+
+  return c.res({
+    image: (
+      <div
+        style={{
+          alignItems: "center",
+          background: "black",
+          display: "flex",
+          height: "100%",
+          justifyContent: "center",
+          width: "100%",
+        }}
+      >
+        <div
+          style={{
+            color: "white",
+            fontSize: 60,
+            textAlign: "center",
+          }}
+        >
+          {status === "response" ? "Hello World!" : "Please Sign In"}
+        </div>
+      </div>
+    ),
+    intents: [
+      <Button>Sign In with Farcaster</Button>,
+      status === "response" && <Button.Reset>Reset</Button.Reset>,
+    ],
+  });
+});
+
+const isProduction = false;
+devtools(app, { serveStatic });
+
+export const GET = handle(app);
+export const POST = handle(app);
